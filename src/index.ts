49e2b5c985c1b5187092867ea570d7f3c9010705
Conor McGrath <conormcgrath123@gmail.com>
2022-02-16 15:37:20 +0000

    updating nodemon config

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Hello world 121!");
 });
 
 // start the Express server
