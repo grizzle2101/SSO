880c875c3dc56fcfc4bc0c26808798be3fb0500f
Starkandco <Starkandco@hotmail.com>
2022-03-05 11:01:25 +0000

    Removing colons

--- src/startup/db.ts
+++ src/startup/db.ts
@@ -1,8 +1,9 @@
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 import mongoose from "mongoose";
 
 export function connectToMongoDB() {
-  dotenv.config();
+  //dotenv.config();
+  require('dotenv').config()
   const db = process.env.DB_URI;
   mongoose.connect(db).then(() => console.log(`Connected to ${db}...`)).catch((error) => console.log('Failed to connect to MongoDB - ' , error));
 };
