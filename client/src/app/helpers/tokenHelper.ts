386b2c74ee5c069ce563af3850d580fa130244f6
Conor McGrath <conormcgrath123@gmail.com>
2022-03-27 21:09:44 +0100

    updating token helper

--- client/src/app/helpers/tokenHelper.ts
+++ client/src/app/helpers/tokenHelper.ts
@@ -1,8 +1,19 @@
 import jwt_decode from 'jwt-decode';
 
-export class TokenHelper{
-    
-    getDecodedAccessToken(token: any): any {
+export class TokenHelper {
+  getToken() {
+    localStorage.getItem('token');
+  }
+
+  storeToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  removeToken() {
+    localStorage.removeItem('token');
+  }
+
+  getDecodedAccessToken(token: any): any {
     try {
       return jwt_decode(token);
     } catch (Error) {
