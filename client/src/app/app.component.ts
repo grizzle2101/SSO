b5c1abd411c3940fd07406cbb3b23e4be77009ba
Starkandco <Starkandco@hotmail.com>
2022-03-30 16:44:19 +0100

    Log out functionality on management panel

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RoutingService } from './services/routing.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  constructor(private routingService: RoutingService) {}
+
+  checkLogin() {
+    if (localStorage.getItem('token')) return true;
+    return false;
+  }
+  logOut() {
+    localStorage.clear();
+    this.routingService.navigateToLogin();
+  }
 }
