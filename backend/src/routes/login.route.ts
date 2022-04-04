aa55367778600608dc05c4e413e3ad42cf852b8e
Conor McGrath <conormcgrath123@gmail.com>
2022-04-04 11:01:58 +0100

    cleaning up route code

--- backend/src/routes/login.route.ts
+++ backend/src/routes/login.route.ts
@@ -1,9 +1,11 @@
+import { Request, Response, NextFunction } from "express";
 import { Router } from "express";
 import userModel from "../models/user.model";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import Joi from "joi";
 import loginModel from "../models/login.model";
+import { User } from "../interfaces/user.interface";
 
 export class LoginRoute {
   public path = "/api/login";
@@ -17,40 +19,52 @@ export class LoginRoute {
   }
 
   private initializeRoutes() {
-    //Perform Login Action
+    //Login Action
     this.router.post(this.path, async (req, res) => {
-      const requestValidity = this.validateRequest(req.body);
-
-      if (requestValidity.error)
-        return res.status(404).send(requestValidity.error.message);
+      const { error } = this.validateRequest(req.body);
+      if (error) return res.status(404).send(error.message);
 
       const user = await this.users.findOne({
-        email: requestValidity.value.email,
+        email: req.body.email,
       });
 
-      if (!user) return res.status(404).send("Invalid email");
-
-      const password = await bcrypt.compare(req.body.password, user.password);
-      if (!password) return res.status(404).send("Incorrect password");
+      await this.checkUserAndPassword(user, req, res);
+      await this.updateLogs(user);
 
       const token = jwt.sign({ user }, this.privateKey);
 
-      console.log("user - ", user);
-      const login = await this.logins.create({
-        user: { _id: user._id, email: user.email },
-        timeStamp: new Date(),
-        origin: "Ireland",
-      });
-
-      res.send({ login, token });
+      res.send({ token });
     });
 
-    //GET Login Resources
+    //GET Logins
     this.router.get(this.path, async (req, res) => {
       const logins = await this.logins.find();
       res.send(logins);
     });
   }
+
+  private async checkUserAndPassword(user: User, req: Request, res: Response) {
+    if (!user) return res.status(404).send("Invalid email");
+
+    const password = await bcrypt.compare(req.body.password, user.password);
+    if (!password) return res.status(404).send("Incorrect password");
+  }
+
+  private updateLogs(user: User) {
+    this.addLogEntry(user);
+    this.addToAggregates();
+  }
+  private async addLogEntry(user: User) {
+    const login = await this.logins.create({
+      user: { _id: user._id, email: user.email },
+      timeStamp: new Date(),
+      origin: "Ireland",
+    });
+  }
+  private addToAggregates() {
+    console.log("aggregating...");
+  }
+
   private validateRequest(request: any) {
     const schema = Joi.object({
       email: Joi.string().min(5).max(320).required().email().lowercase(),
