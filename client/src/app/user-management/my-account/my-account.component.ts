5db6eb8a9dd617d1c00c72f6c8daba467a21d42b
Conor McGrath <conormcgrath123@gmail.com>
2022-04-10 19:58:55 +0100

    setting up account form

--- client/src/app/user-management/my-account/my-account.component.ts
+++ client/src/app/user-management/my-account/my-account.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { RoutingService } from 'src/app/services/routing.service';
+import { TokenService } from 'src/app/services/token.service';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
@@ -11,8 +13,27 @@ export class MyAccountComponent implements OnInit {
   isEdit: boolean = false;
   isLoading: boolean = true;
 
+  accountForm = new FormGroup({
+    userName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(255),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(255),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(255),
+    ]),
+  });
+
   constructor(
     private routingService: RoutingService,
+    public tokenService: TokenService,
     private userService: UsersService
   ) {}
 
