60511ccb7cf6657f911a8e932da77c3c381e4bbe
Conor McGrath <conormcgrath123@gmail.com>
2022-04-17 12:55:25 +0100

    adding MJML templates

--- backend/src/services/EmailService.ts
+++ backend/src/services/EmailService.ts
@@ -1,8 +1,14 @@
 import nodemailer from "nodemailer";
+import fs from "fs";
+import mjml2html from "mjml";
 
 export class EmailService {
   private emailAccount = process.env.EMAIL_ACCOUNT;
   private emailPassword = process.env.EMAIL_PASSWORD;
+  private templateFolder = `${__dirname}/../templates/`;
+
+  private createAccountPath = `${this.templateFolder}/create-account-template.mjml`;
+  private accountTemplate = fs.readFileSync(this.createAccountPath).toString();
 
   smtpTransport = nodemailer.createTransport(
     `smtps://${this.emailAccount}:` +
@@ -11,11 +17,13 @@ export class EmailService {
   );
 
   async sendEmail(recipient: string, subject: string, message: string) {
+    const template = mjml2html(this.accountTemplate);
+
     var mailOptions = {
       from: this.emailAccount,
       to: recipient,
       subject: subject,
-      text: message,
+      html: template.html,
     };
 
     return await this.smtpTransport.sendMail(mailOptions);
